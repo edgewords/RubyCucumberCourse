7810f7dac7699077c7835e3ccb1c05fa7fb400e9
Tom Millichamp <tom@edgewords.co.uk>
2019-12-10 16:26:23 +0000

    added check for response 200

--- features/step_definitions/apiDemoSteps.rb
+++ features/step_definitions/apiDemoSteps.rb
@@ -13,10 +13,14 @@ require 'httparty'
 
 When("I request API information on employee {string}") do |employeeID|
     response = HTTParty.get('http://dummy.restapiexample.com/api/v1/employee/'+employeeID)
+    @responseCode = response.code
+    puts @responseCode.class
+
     # below returns a string
     @strResponse = response.body # {"id":"1","employee_name":"Abhi Deepak C","employee_salary":"1000","employee_age":"23","profile_image":""
   end
   
   Then("The Customer Name is {string}") do |custName|
+    expect(@responseCode).to eq(200) #check http response is OK (200)
     expect(@strResponse).to include(custName)
   end
